b741bf14ddb2649d8f08e337dd6a3bd24495f733
Nick Maharjan <nick.maharjan.758@gmail.com>
2025-11-10 18:21:46 -0800

    variable operator and control statement

--- myscript.js
+++ myscript.js
@@ -29,7 +29,7 @@ function multiplication(){
 
 function division(){
     let a = 20;
-    let b = 5;
+    let b = 50;
     let c = a / b;
     document.getElementById("div").innerText = "Quotient: " + c;
 }
